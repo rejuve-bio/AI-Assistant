ea4f2a036fd05c8122eb2d6253e9883e2f1bc0ad
kewserseid <kewerseid91@gmail.com>
2024-10-07 16:52:14 +0300

    api for graph summarizer

--- app/routes.py
+++ app/routes.py
@@ -3,6 +3,7 @@ from flask import Blueprint, request, jsonify, current_app
 from dotenv import load_dotenv
 from .services.ai_assistant import AIAssistantSystem
 from .services.llm_models import GeminiModel, OpenAIModel
+from .services.graph_handler import Graph_Summarizer
 
 import requests
 import os
@@ -34,6 +35,23 @@ def query_knowledge_graph(kg_service_url, json_query):
         print(f"Error with JSON query structure: {e}")
         return {"error": str(e)}
     
+def get_llm_model(config):
+    model_type = config['llm_model']
+
+    if model_type == 'openai':
+        openai_api_key = os.getenv('OPENAI_API_KEY')
+        if not openai_api_key:
+            raise ValueError("OpenAI API key not found")
+        return OpenAIModel(openai_api_key)
+    elif model_type == 'gemini':
+        gemini_api_key = os.getenv('GEMINI_API_KEY')
+        if not gemini_api_key:
+            raise ValueError("Gemini API key not found")
+        return GeminiModel(gemini_api_key)
+    else:
+        raise ValueError("Invalid model type in configuration")
+
+   
 
 @main_bp.route('/query', methods=['POST'])
 def process_query():
@@ -44,17 +62,12 @@ def process_query():
         return jsonify({"error": "No query provided"}), 400
 
     config = current_app.config
-    model_type = config['llm_model']
     schema_text = open(config['schema_path'], 'r').read()
     
-    if model_type == 'openai':
-        openai_api_key = os.getenv('OPENAI_API_KEY')
-        llm = OpenAIModel(openai_api_key)
-    elif model_type == 'gemini':
-        gemini_api_key = os.getenv('GEMINI_API_KEY')
-        llm = GeminiModel(gemini_api_key)
-    else:
-        return jsonify({"error": "Invalid model type in configuration"}), 500
+    try:
+        llm = get_llm_model()
+    except ValueError as e:
+        return jsonify({"error": str(e)}), 500
 
     ai_system = AIAssistantSystem(llm, schema_text)
     json_query = ai_system.process_query(query)
@@ -68,3 +81,23 @@ def process_query():
     final_response = ai_system.process_kg_response(query, json_query, kg_response)
 
     return jsonify(final_response)
+
+
+
+@main_bp.route('/summarizer', methods=['POST'])
+def summarize_graph():
+    data = request.json
+    graph = data.get('graph')
+
+    if not graph:
+        return jsonify({"error": "No Valid Graph is provided"}), 400
+    try:
+        config = current_app.config
+        llm = get_llm_model(config)
+    except ValueError as e:
+        return jsonify({"error": str(e)}), 500
+
+    summary = Graph_Summarizer(llm)
+    graph_summary = summary.open_ai_summarizer(graph)
+    return jsonify(graph_summary)
+          
