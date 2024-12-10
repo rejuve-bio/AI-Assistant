0bc8a92829920a6dcdfddb56c31f438d6885136d
kewserseid <kewerseid91@gmail.com>
2024-12-10 16:59:39 +0300

    auth

--- tests/test_routes_rag.py
+++ tests/test_routes_rag.py
@@ -3,6 +3,7 @@ import pytest
 import json
 import os
 from app import create_app
+from helper.access_token_generator import access_token_generator
 
 @pytest.fixture
 def app():
@@ -13,11 +14,11 @@ def test_base_route(app):
     client = app.test_client()
     client = app.test_client()
     url = '/query'
-    auth_token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoic29tZV9pZCJ9.bLKE-JJSFBT9jJjd9_002lUDiikbvI2RoBkSJs0mTDk'
+    token = access_token_generator()
     query = "get me the summary of the pdf?"
     user = "4"
 
-    response = client.post(url, json={'query': query,'user':user}, headers={'Authorization':auth_token})
+    response = client.post(url, json={'query': query,'user':user}, headers={'Authorization':token})
     print("***",response.data)
     print("***",response.status_code)
     assert response.status_code == 200
